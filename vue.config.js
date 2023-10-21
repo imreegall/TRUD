499359a7e5263b2edde6136ed83ffff86a174724
Galiev Renat <imreegall@gmail.com>
2023-10-21 15:34:58 +0300

    Add styles, clear default components

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+              @import "/src/assets/styles/_mixins.sass"
+              @import "/src/assets/styles/_variables.sass"
+            `,
+      },
+    },
+  },
 })
